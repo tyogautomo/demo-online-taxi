d7d6feebe0a1a9544738aa2ef28f76ca6bc48d34
tyogautomo <yoga.utomo27@gmail.com>
2020-03-09 14:56:48 +0700

    custom tab abr

--- app/containers/Home/Home.screen.js
+++ app/containers/Home/Home.screen.js
@@ -1,12 +1,80 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 class Home extends Component {
+  offset = 0;
+  componentDidMount() {
+    this.setInitialParam();
+  }
+
+  onScroll = (e) => {
+    const { navigation } = this.props;
+    const currentOffsetY = e.nativeEvent.contentOffset.y;
+    if (currentOffsetY > this.offset) {
+      navigation.setParams({ isTabVisible: false })
+    }
+    if (currentOffsetY < this.offset) {
+      navigation.setParams({ isTabVisible: true })
+    }
+    this.offset = currentOffsetY;
+  }
+
+  setInitialParam = () => {
+    const { navigation } = this.props;
+    navigation.setParams({ isTabVisible: true })
+  };
+
   render() {
     return (
-      <View>
+      <ScrollView onScroll={this.onScroll}>
         <Text>This is home screen</Text>
-      </View>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+        <Text>This is home screen</Text>
+      </ScrollView>
     )
   }
 }
---
+++ app/themes/colors.js
@@ -0,0 +1,6 @@
+const colors = {
+  salem: '#039A34',
+  silver: '#c2c2c2'
+}
+
+export { colors };
